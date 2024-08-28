13be05eabca8253283a8134f4842ea43a50c3b5d
asrivastavrangers <asrivastav@sandiego.edu>
2024-08-28 11:48:17 +0530

    Added

--- todobackend/settings.py
+++ todobackend/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 ROOT_URLCONF = 'todobackend.urls'
 
